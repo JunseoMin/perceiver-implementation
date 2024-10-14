958be8a7e47801df7eb243e7b305e320a7ec57d4
JunsoMin <minjs4562@gmail.com>
2024-10-14 16:15:42 +0900

    fix: args

--- include/transformer/layer.py
+++ include/transformer/layer.py
@@ -3,10 +3,10 @@ from sublayer import PositionWiseFeedFoward
 from sublayer import MultiheadAttention
 
 class EncoderLayer(nn.Module):
-    def __init__(self, dim_model, dim_input, dim_hidden, n_head, d_k,d_v, dropout = 0.1):
+    def __init__(self, dim_model, dim_inner, n_head, d_k, d_v, dropout = 0.1):
         super().__init__()
         self.atten = MultiheadAttention(n_head, dim_model, d_k, d_v, dropout=dropout)
-        self.FFN = PositionWiseFeedFoward(dim_input, dim_hidden, dropout=dropout)
+        self.FFN = PositionWiseFeedFoward(dim_model, dim_inner, dropout=dropout)
     
     def foward(self,enc_in, mask = None):
         output = self.atten(enc_in, mask)
@@ -15,12 +15,12 @@ class EncoderLayer(nn.Module):
 
 
 class DecoderLayer(nn.Module):
-    def __init__(self, dim_model, dim_input, dim_hidden, n_head, h, dropout = 0.1):
+    def __init__(self, dim_model, dim_inner, n_head, d_k, d_v, dropout = 0.1):
         super().__init__()
 
-        self.atten1 = MultiheadAttention(n_head, dim_model, h, dropout=dropout)
-        self.atten2 = MultiheadAttention(n_head, dim_model, h, dropout=dropout)
-        self.FFN = PositionWiseFeedFoward(dim_input, dim_hidden, dropout=dropout)
+        self.atten1 = MultiheadAttention(n_head, dim_model, d_k, d_v, dropout=dropout)
+        self.atten2 = MultiheadAttention(n_head, dim_model, d_k, d_v, dropout=dropout)
+        self.FFN = PositionWiseFeedFoward(dim_model, dim_inner, dropout=dropout)
 
     def foward(self, dec_in, enc_out, dec_atten_mask = True, coder_mask = None):
         dec_out = self.atten1(dec_in, dec_in, dec_in, mask = dec_atten_mask) # masked self-attention
