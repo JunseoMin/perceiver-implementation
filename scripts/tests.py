29f3687b2eddbae4d9b0297731b0a42a6d5e61df
JunsoMin <minjs4562@gmail.com>
2024-10-17 15:27:42 +0900

    final testcode

--- scripts/tests.py
+++ scripts/tests.py
@@ -9,6 +9,7 @@ from include.transformer.modules import ScaledDotProductAttention
 import modules as pm
 
 from layers import PerceiverLayer
+from perceiver import Perceiver
 
 def test_cross_attention():
     # Parameters for the CrossAttention module
@@ -20,7 +21,7 @@ def test_cross_attention():
     temp = 1
 
     # Initialize CrossAttention module
-    cross_attention = pm.CrossAttention(n_head, d_byte_arr, d_latent, d_k, d_v, temp)
+    cross_attention = pm.Attention(n_head, d_byte_arr, d_latent, d_k, d_v, temp)
 
     # Sample input tensors for q, k, and v
     batch_size = 2
@@ -63,7 +64,43 @@ def test_layers():
     assert output.shape == latent.shape, "Output shape mismatch"
     print("Test passed!")
 
+def test_perceiever():
+    share_weights = True
+    depth = 6
+    n_classes = 10
+    in_axis = [32, 32]
+    max_freq = 10
+    n_head = 4
+    d_head = 64
+    d_byte_arr = 2048
+    d_latent = 256
+    n_latent = 512
+    d_kv = 64
+    input_type = torch.float32
+    device = 'cpu'  
+    n_bands = 4
+    atten_dropout = 0.1
+    ff_dropout = 0.1
+
+    
+    model = Perceiver(share_weights, depth, n_classes, in_axis, max_freq, n_head, d_head, d_byte_arr, d_latent,
+                      n_latent, d_kv, input_type, device, n_bands, atten_dropout, ff_dropout)
+
+    
+    batch_size = 8
+    input_data = torch.randn(batch_size, *in_axis, d_byte_arr)
+
+    
+    output = model(input_data)
+
+    
+    print("Output shape:", output.shape)  # (batch_size, n_classes)
+
+    assert output.shape == (batch_size, n_classes), f"Output shape mismatch: expected ({batch_size}, {n_classes}), but got {output.shape}"
+    print("Test passed!")
+
+
 
 if __name__ == '__main__':
     # Run the test
-    test_layers()
+    test_perceiever()
