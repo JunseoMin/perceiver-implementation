d8ed3d532b54cadaba3d35c25bc454622177e3c2
JunsoMin <minjs4562@gmail.com>
2024-10-17 11:52:52 +0900

    init perceiver model

--- scripts/layers.py
+++ scripts/layers.py
@@ -16,14 +16,14 @@ from include.transformer.sublayer import PositionWiseFeedforward
 
 class PerceiverLayer(nn.Module):
     def __init__(self,max_freq, n_head, d_head, d_byte_arr, d_latent, d_kv, input_type, device , 
-                 num_bands = 4, cross_atten_dropout = 0.1,self_atten_dropout = 0.1, 
-                 cross_ff_dropout =0.1, latent_ff_dropout = 0.1):
+                 num_bands = 4, atten_dropout = 0.1,ff_dropout = 0.1):
+        
         super().__init__()
-        self.cross_atten = Attention(n_head, d_byte_arr, d_latent, d_kv, d_kv, temp=d_head ** -0.5, dropout=cross_atten_dropout)
-        self.self_atten = LatentTransformer(n_head, d_head, d_kv, d_latent, self_atten_dropout)
+        self.cross_atten = Attention(n_head, d_byte_arr, d_latent, d_kv, d_kv, temp=d_head ** -0.5, dropout=atten_dropout)
+        self.self_atten = LatentTransformer(n_head, d_head, d_kv, d_latent, atten_dropout)
         self.positional_encoding = FourierFeaturePositionEncoding(max_freq, d_byte_arr, input_type, device, num_bands)
-        self.byte_FFN = PositionWiseFeedforward(d_latent,dropout=cross_ff_dropout)
-        self.latent_FFN = PositionWiseFeedforward(d_latent,dropout=latent_ff_dropout)
+        self.byte_FFN = PositionWiseFeedforward(d_latent,dropout=ff_dropout)
+        self.latent_FFN = PositionWiseFeedforward(d_latent,dropout=ff_dropout)
 
     def forward(self, latent, byte_arr):
         #pass layers
--- scripts/perceiver.py
+++ scripts/perceiver.py
@@ -1,12 +1,24 @@
 import torch.nn as nn
+from layers import PerceiverLayer
+
+from einops import rearrange, repeat
+from einops.layers.torch import Reduce
 
 '''
 Implementation of model
 '''
 
 class Perceiver(nn.Module):
-    def __init__(self):
+    def __init__(self, depth, weight_sharing, atten_dropout, ff_dropout, input_channel, n_cross_head, n_latent_heads, n_classes, d_latent  ):
         super().__init__()
-    
+
+
+        self.to_logits = nn.Sequential(
+            Reduce('b n d -> b d', 'mean'),
+            nn.LayerNorm(d_latent),
+            nn.Linear(d_latent, n_classes)
+        ) 
+
+
     def forward(self):
         pass
