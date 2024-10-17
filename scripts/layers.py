be27f07db2735fabf8f23680e797c45a230580d8
JunsoMin <minjs4562@gmail.com>
2024-10-17 10:27:45 +0900

    feat: perceiever layer

--- scripts/layers.py
+++ scripts/layers.py
@@ -5,14 +5,16 @@ import sys,os
 import torch.nn as nn
 import torch
 
+from latenttransformer import LatentTransformer
+
 from modules import PAttention,FourierFeaturePositionEncoding
 from einops import rearrange
+
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from include.transformer.sublayer import PositionWiseFeedforward
 
-
-class CrossAttention(nn.Module):
+class Attention(nn.Module):
     def __init__(self, n_head, d_byte_arr, d_latent, d_k, d_v, temp, dropout = 0.1):
         super().__init__()
         self.n_head , self.d_byte_arr, self.d_k, self.d_v, self.d_latent = n_head, d_byte_arr, d_k, d_v, d_latent
@@ -61,13 +63,23 @@ class CrossAttention(nn.Module):
 
 
 class PerceiverLayer(nn.Module):
-    def __init__(self,max_freq, n_head, d_byte_arr, d_latent, d_kv, input_type, device , num_bands = 4, temp = 0.1, dropout = 0.1):
+    def __init__(self,max_freq, n_head, d_head, d_byte_arr, d_latent, d_kv, input_type, device , num_bands = 4, dropout = 0.1):
         super().__init__()
-        self.crossatten = CrossAttention(n_head, d_byte_arr, d_latent, d_kv, d_kv, temp=temp, dropout=dropout)
+        self.cross_atten = Attention(n_head, d_byte_arr, d_latent, d_kv, d_kv, temp=d_head ** -0.5, dropout=dropout)
+        self.self_atten = LatentTransformer(n_head, d_head, d_kv, d_latent, dropout)
         self.positional_encoding = FourierFeaturePositionEncoding(max_freq, d_byte_arr, input_type, device, num_bands)
-        self.FFN = PositionWiseFeedforward(d_byte_arr,d_latent,dropout=dropout)
+        self.byte_FFN = PositionWiseFeedforward(d_latent,dropout=dropout)
+        self.latent_FFN = PositionWiseFeedforward(d_latent,dropout=dropout)
 
+        self.layers = nn.ModuleList([self.cross_atten, self.byte_FFN , self.self_atten, self.latent_FFN])
 
-    def forward(self, x):
+    def forward(self, latent, byte_arr):
 
-        pass
+        #pass layers
+        output = self.cross_atten(latent,byte_arr,byte_arr)
+        output = self.byte_FFN(output)
+
+        output = self.self_atten(output,output,output)
+        output = self.latent_FFN(output)
+
+        return output
